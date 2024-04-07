6824d1a96515d92ef09c245f537b6e547b1975c4
Evis Drenova <evis@usenucleus.cloud>
2024-04-06 21:20:23 -0700

    remove ph for now

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode, Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import { kcContext as kcAccountThemeContext } from "./account/kcContext";
 import { kcContext as kcLoginThemeContext } from "./login/kcContext";
-import PHProvider from "./PosthogProvider";
 
 const KcLoginThemeApp = lazy(() => import("./login/KcApp"));
 const KcAccountThemeApp = lazy(() => import("./account/KcApp"));
@@ -10,22 +9,20 @@ const KcAccountThemeApp = lazy(() => import("./account/KcApp"));
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Suspense>
-      <PHProvider>
-        {(() => {
-          if (kcLoginThemeContext !== undefined) {
-            return <KcLoginThemeApp kcContext={kcLoginThemeContext} />;
-          }
+      {(() => {
+        if (kcLoginThemeContext !== undefined) {
+          return <KcLoginThemeApp kcContext={kcLoginThemeContext} />;
+        }
 
-          if (kcAccountThemeContext !== undefined) {
-            return <KcAccountThemeApp kcContext={kcAccountThemeContext} />;
-          }
+        if (kcAccountThemeContext !== undefined) {
+          return <KcAccountThemeApp kcContext={kcAccountThemeContext} />;
+        }
 
-          throw new Error(
-            "This app is a Keycloak theme" +
-              "It isn't meant to be deployed outside of Keycloak"
-          );
-        })()}
-      </PHProvider>
+        throw new Error(
+          "This app is a Keycloak theme" +
+            "It isn't meant to be deployed outside of Keycloak"
+        );
+      })()}
     </Suspense>
   </StrictMode>
 );
