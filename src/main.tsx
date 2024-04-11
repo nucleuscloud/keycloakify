0ae16c5ec4ac8d1ab9154394cd274ae04ad7424b
Evis Drenova <80707987+evisdrenova@users.noreply.github.com>
2024-04-10 18:34:12 -0700

    add js script (#15)

--- src/main.tsx
+++ src/main.tsx
@@ -6,34 +6,23 @@ import { kcContext as kcLoginThemeContext } from "./login/kcContext";
 const KcLoginThemeApp = lazy(() => import("./login/KcApp"));
 const KcAccountThemeApp = lazy(() => import("./account/KcApp"));
 
-import { PostHogProvider } from "posthog-js/react";
-
-const options = {
-  api_host: "https://app.posthog.com",
-};
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Suspense>
-      <PostHogProvider
-        apiKey="phc_yc5YUhGheWJL8XSKlLQJ9DYOH5Fi1IQyi7RB1TD4eyAY"
-        options={options}
-      >
-        {(() => {
-          if (kcLoginThemeContext !== undefined) {
-            return <KcLoginThemeApp kcContext={kcLoginThemeContext} />;
-          }
+      {(() => {
+        if (kcLoginThemeContext !== undefined) {
+          return <KcLoginThemeApp kcContext={kcLoginThemeContext} />;
+        }
 
-          if (kcAccountThemeContext !== undefined) {
-            return <KcAccountThemeApp kcContext={kcAccountThemeContext} />;
-          }
+        if (kcAccountThemeContext !== undefined) {
+          return <KcAccountThemeApp kcContext={kcAccountThemeContext} />;
+        }
 
-          throw new Error(
-            "This app is a Keycloak theme" +
-              "It isn't meant to be deployed outside of Keycloak"
-          );
-        })()}
-      </PostHogProvider>
+        throw new Error(
+          "This app is a Keycloak theme" +
+            "It isn't meant to be deployed outside of Keycloak"
+        );
+      })()}
     </Suspense>
   </StrictMode>
 );
